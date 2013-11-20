5b144cebb6800a68523c113e0c1ebf734e2552c8
Matt Conway <matt@conwaysplace.com>
2013-11-20 08:33:30 -0500

    include app name in procline

--- lib/trident/pool_manager.rb
+++ lib/trident/pool_manager.rb
@@ -19,7 +19,7 @@ module Trident
       pools.each do |pool|
         pool.start
       end
-      procline "manager", "managing #{procline_display}"
+      procline "manager-#{name}", "managing #{procline_display}"
     end
 
     def stop_forcefully
@@ -33,7 +33,7 @@ module Trident
     # waits for children to exit
     def wait
       logger.info "Waiting for pools to exit"
-      procline "manager", "waiting #{procline_display}"
+      procline "manager-#{name}", "waiting #{procline_display}"
       pools.each do |pool|
         pool.wait
       end
@@ -44,22 +44,17 @@ module Trident
       pools.each do |pool|
         pool.update
       end
-      procline "manager", "managing #{procline_display}"
+      procline "manager-#{name}", "managing #{procline_display}"
     end
 
     private
 
     def procline_display
-      display = ""
-      pools.each_with_index do |pool, i|
-        display << "#{pool.name}#{pool.workers.to_a.inspect}"
-        display << " " if i + 1 < pools.size
-      end
-      display
+      pools.collect {|pool| "#{pool.name}#{pool.workers.to_a.inspect}" }.join(" ")
     end
 
     def load_handlers
-      procline "manager", "preforking #{procline_display}"
+      procline "manager-#{name}", "preforking #{procline_display}"
       pools.each do |pool|
         pool.handler.load
       end
@@ -68,7 +63,7 @@ module Trident
     # tells all children to stop using action
     def stop(action)
       logger.info "Stopping pools: #{action}"
-      procline "manager", "stopping #{procline_display}"
+      procline "manager-#{name}", "stopping #{procline_display}"
       pools.each do |pool|
         pool.stop(action)
       end
